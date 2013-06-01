c3d138fffc620be9117974848b3c70bb9d5988ef
rastating <intninety.nospam@gmail.com>
2013-06-01 23:27:29 +0100

    Updated version number

--- js/jquery.graphly.js
+++ js/jquery.graphly.js
@@ -1,6 +1,6 @@
 // Copyright (C) 2013 rastating
 //
-// Version 0.1.4
+// Version 0.2.4
 //
 // This program is free software: you can redistribute it and/or modify
 // it under the terms of the GNU General Public License as published by
